0052d79d353c1aa15f9e7cba7afdccfd4a3db4b6
pigamers <pigamers1123@gmail.com>
2025-10-21 23:16:56 +0530

    updated api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://nurture-backend-szln.onrender.com/';
+const API_URL = 'https://nurture-backend-szln.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
